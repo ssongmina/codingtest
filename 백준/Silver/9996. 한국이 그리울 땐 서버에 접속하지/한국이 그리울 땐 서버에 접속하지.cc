b1374dcb283a4c2b90b21116d291c9a569e8a352
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-05-05 11:32:01 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+string ptn, tmp, front, en;
+
+int main(){
+	cin >> n;
+	cin >> ptn;
+	
+	int it = ptn.find("*");
+	front = ptn.substr(0, it);
+	en = ptn.substr(it+1);
+	
+	for(int i=0; i<n; i++){
+		cin >> tmp;
+		if(tmp.size() < front.size() + en.size()) cout << "NE\n";
+		else{
+            if(tmp.substr(0, front.size()) == front && tmp.substr(tmp.size() - en.size()) == en)
+			    cout << "DA\n";
+		    else cout << "NE\n";
+        }
+     }
+	return 0;
+}
