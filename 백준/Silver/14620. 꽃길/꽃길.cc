e6a54c5a704b995366b4dcb91d588c6aa3444021
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-05-22 13:57:53 +0900

    [Silver II] Title: 꽃길, Time: 44 ms, Memory: 15412 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.cc
+++ 백준/Silver/14620. 꽃길/꽃길.cc
@@ -2,56 +2,42 @@
 
 using namespace std;
 
-int n, arr[15][15], visited[15][15], price=987654321;
+int n, mn=987654321;
+int land[15][15], visited[15][15];
+vector<vector<pair<int,int>>> seed;
+vector<pair<int,int>> v, spot;
 int dy[4] = {-1,0,1,0}, dx[4] = {0,1,0,-1};
 
-bool check(int y, int x){
-	if(visited[y][x]) return false;
-	for(int i=0; i<4; i++){
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if(ny < 0 || nx < 0 || ny >= n || nx >= n) return false;
-		if(visited[ny][nx]) return false;
+void combi(int idx, vector<pair<int,int>> spot){
+	if(spot.size() == 3){
+		seed.push_back(spot);
+		return ;
 	}
-	return true;
-}
-
-int setFlo(int y, int x){
-	visited[y][x] = 1;
-	
-	int cnt = arr[y][x];
-	for(int i=0; i<4; i++){
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		cnt += arr[ny][nx];
-		visited[ny][nx] = 1;
-	}
-	return cnt;
-}
-
-void eraseFlo(int y, int x){
-	visited[y][x] = 0;
-	for(int i=0; i<4; i++){
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		visited[ny][nx] = 0;
+	for(int i=idx+1; i<v.size(); i++){
+		spot.push_back(v[i]);
+		combi(i, spot);
+		spot.pop_back();
 	}
 	return ;
 }
 
-void go(int cnt, int ret){
-	if(cnt == 3){
-		price = min(price, ret);
-		return ;
-	}
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
-			if(check(i,j)){
-				go(cnt+1, ret+setFlo(i,j));
-				eraseFlo(i,j);
-			}
+void check(vector<pair<int,int>> a){
+	int sum = 0;
+	memset(visited, 0, sizeof(visited));
+	for(auto k : a){
+		if(visited[k.first][k.second]) return ;
+		visited[k.first][k.second] = 1;
+		sum += land[k.first][k.second];
+		for(int i=0; i<4; i++){
+			int ny = k.first + dy[i];
+			int nx = k.second + dx[i];
+			if(ny < 0 || nx < 0 || ny >= n || nx >= n) return ;
+			if(visited[ny][nx]) return ;
+			visited[ny][nx] = 1;
+			sum += land[ny][nx];
 		}
 	}
+	mn = min(sum, mn);
 	return ;
 }
 
@@ -59,16 +45,28 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
+	// 입력 
 	cin >> n;
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
-			cin >> arr[i][j];
+			cin >> land[i][j];
 		}
 	}
-	
-	go(0,0);
-	
-	cout << price << "\n";
+	// 씨앗 3개 심을 곳 정하기 
+
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			v.push_back({i,j});
+		}
+	}
+	combi(-1, spot);
+
+	// 최소 가격인지 확인
+	for(auto a : seed){
+		check(a);
+	}
+	 
+	cout << mn << "\n";
 	
 	return 0;
 }
