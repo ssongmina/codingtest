63af63465d2eb2f96b2acbcb2b2101efbacbe330
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-05-27 15:53:28 +0900

    [Silver V] Title: 집합, Time: 624 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -2,40 +2,47 @@
 
 using namespace std;
 
+int m,x;
 string str;
-int x, m;
-int val = 0;
+vector<int> v;
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> m;
-	while(m--){
+	for(int i=0; i<m; i++){
 		cin >> str;
 		if(str == "add"){
 			cin >> x;
-			val |= (1<<x);
+			if(find(v.begin(), v.end(), x) == v.end()) v.push_back(x);
 		}
 		else if(str == "remove"){
 			cin >> x;
-			val &= ~(1<<x);
+			auto a = find(v.begin(), v.end(), x);
+			if(a != v.end()){
+				v.erase(a);
+			}
 		}
 		else if(str == "check"){
 			cin >> x;
-			if(val & (1<<x)) cout << "1\n";
-			else cout << "0\n";
+			if(find(v.begin(), v.end(), x) == v.end()) cout << "0\n";
+			else cout << "1\n";
 		}
 		else if(str == "toggle"){
 			cin >> x;
-			if(val & (1<<x)) val &= ~(1<<x);
-			else val |= (1<<x);
+			auto a = find(v.begin(), v.end(), x);
+			if(a == v.end()) v.push_back(x);
+			else v.erase(a);
 		}
 		else if(str == "all"){
-			val = (1<<21) - 1;
+			v.clear();
+			for(int i=1; i<=20; i++){
+				v.push_back(i);
+			}
 		}
 		else if(str == "empty"){
-			val = 0;
+			v.clear();
 		}
 	}
 	
