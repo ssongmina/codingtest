726c5da7b4f965b401da95ee206a97c86dca7a6d
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-05-29 10:22:53 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string str;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> str;
+	cout << str << "??!";
+	
+	return 0;
+}
