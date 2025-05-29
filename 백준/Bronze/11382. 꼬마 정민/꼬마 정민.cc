8a91a910fe04b77e0cbb1250af4e1a7a42f16e00
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-05-29 10:20:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long a,b,c;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> a >> b >> c;
+	cout << a + b + c ;
+	
+	return 0;
+}
