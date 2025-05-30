5dd58347149876c76742ad9a0398c3c3b5d6d2f6
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-05-30 10:16:55 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long double a,b;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> a >> b;
+	
+	
+	cout.precision(15);
+    cout << fixed; 
+    cout << a / b << endl;
+	
+	return 0;
+}
