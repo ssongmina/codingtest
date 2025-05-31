74edc6488ae5c83af4ee31514fdb231ebd48f690
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-05-31 10:30:05 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,a,b;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
+	cout << n*(n+1)/2 << "\n";
+	
+	return 0;
+}
