694f181fe32b6b3bccc0974ec17c4c64318572fa
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-06-02 10:36:18 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+string str;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> str >> n;
+	cout << str[n-1] << "\n";
+	
+	
+	return 0;
+}
