ad5b68c9ba429181d97f44b1cee908709c588e6f
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-06-02 10:36:53 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+string str;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> str;
+	cout << str.size() << "\n";
+	
+	
+	return 0;
+}
