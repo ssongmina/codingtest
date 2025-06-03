63e278de9dc972dd9b380a200e1ea8f7f943f48a
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-06-03 11:43:18 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string str;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+	
+	return 0;
+}
