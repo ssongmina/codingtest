71816862809ab6924455cee35416cfaf577c3595
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-06-08 11:05:20 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a,b,v;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> a >> b >> v;
+	
+	cout << (v-b-1)/(a-b) + 1;
+	
+	return 0;
+}
