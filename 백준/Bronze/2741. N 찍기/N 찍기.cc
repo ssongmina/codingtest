13d3cd37429035c112c7fdf9e6170734d8122cf7
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-06-27 11:40:08 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
+	for(int i=1; i<=n; i++) cout << i << "\n";
+	return 0;
+}
