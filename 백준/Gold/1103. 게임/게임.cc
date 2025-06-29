4a9d9a0ce64b10139f62776ee25c9622e1949812
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-06-29 20:59:46 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -4,27 +4,28 @@ using namespace std;
 
 int n,m,ret;
 string str;
-int visited[55][55], board[55][55], dp[55][55];
+char arr[55][55];
+int dp[55][55], visited[55][55];
 int dy[4] = {-1,0,1,0}, dx[4] = {0,1,0,-1};
 
 int go(int y, int x){
-	if(y < 0 || x < 0 || y >= n || x >= m || board[y][x] == 0) return 0;
+	if(y < 0 || x < 0 || y >= n || x >= m || arr[y][x] == 'H') return 0;
 	if(visited[y][x]){
 		cout << "-1\n";
 		exit(0);
 	}
-	if (dp[y][x]) return dp[y][x];
-
-    visited[y][x] = true;
-    int max_cnt = 0, step = board[y][x];
-    for (int i = 0; i < 4; i++) {
-        int ny = y + dy[i] * step;
-        int nx = x + dx[i] * step;
-        max_cnt = max(max_cnt, go(ny, nx) + 1);
-    }
-    visited[y][x] = false;
-
-    return dp[y][x] = max_cnt;
+	
+	if(dp[y][x]) return dp[y][x];
+	
+	visited[y][x] = 1;
+	int move = arr[y][x] - '0';
+	for(int i=0; i<4; i++){
+		int ny = y + dy[i] * move;
+		int nx = x + dx[i] * move;
+		dp[y][x] = max(dp[y][x], go(ny,nx) + 1);
+	}
+	visited[y][x] = 0;
+	return dp[y][x];
 }
 
 int main(){
@@ -35,12 +36,11 @@ int main(){
 	for(int i=0; i<n; i++){
 		cin >> str;
 		for(int j=0; j<m; j++){
-			if(str[j] == 'H') board[i][j] = 0;
-			else board[i][j] = str[j] - '0';
+			arr[i][j] = str[j];
 		}
 	}
 	
 	cout << go(0,0) << "\n";
 	
 	return 0;
-} 
+}
