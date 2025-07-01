0a04f99955aa843432b33c089bf142e5d0a0a44d
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-07-01 11:40:44 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -2,32 +2,26 @@
 
 using namespace std;
 
-int n,k,x;
-vector<int> v;
-int dp[10005];
+int n,k,a;
+int arr[105], dp[10005];
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n >> k;
-	for(int i=0; i<n; i++){
-		cin >> x;
-		v.push_back(x);
-	}
-	fill(dp, dp+10005, 1e9);
 	
+	fill(dp, dp+10005, 987654321);
 	dp[0] = 0;
-	for(auto a : v){
-		for(int i=a; i<k+1; i++){
-			if(i-a < 0) continue;
-			dp[i] = min(dp[i], dp[i-a]+1);
+	for(int i=0; i<n; i++){
+		cin >> a;
+		for(int j=a; j<=k; j++){
+			dp[j] = min(dp[j], dp[j-a]+1);
 		}
 	}
 	
-	if(dp[k] == 1e9) cout << "-1\n";
+	if(dp[k] == 987654321) cout << "-1\n";
 	else cout << dp[k] << "\n";
-	
 	
 	return 0;
 }
