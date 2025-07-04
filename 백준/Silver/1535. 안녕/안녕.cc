91987417619551ab0ee821c62ba791ec4de14023
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-07-04 11:36:35 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -3,22 +3,26 @@
 using namespace std;
 
 int n;
-int l[25], j[25], dp[105];
+int l[25], jj[25], dp[105];
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n;
-	for(int i=0; i<n; i++) cin >> l[i];
-	for(int i=0; i<n; i++) cin >> j[i];
+	for(int i=0; i<n; i++) cin >> l[i]; 
+	for(int i=0; i<n; i++) cin >> jj[i];
 	
+	dp[100] = 0; // 체력이 100일 떄 기쁨은 0
 	for(int i=0; i<n; i++){
-		for(int k=100; k>l[i]; k--){
-			dp[k] = max(dp[k], dp[k-l[i]]+j[i]);
+		for(int j=100; j>l[i]; j--){
+			dp[j] = max(dp[j], dp[j-l[i]] + jj[i]);
 		}
-	}
+	} 
+	
 	cout << dp[100] << "\n";
+		
+	
 	
 	return 0;
 }
