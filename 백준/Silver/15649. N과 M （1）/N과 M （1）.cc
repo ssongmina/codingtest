7bd081e0eeb99457f315e0e43f5c2d59d329ec30
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-07-08 16:07:58 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -3,14 +3,26 @@
 using namespace std;
 
 int n,m;
-vector<vector<int>> ret, sum;
+int visited[15], arr[15];
 
-vector<int> check(int idx){
-	vector<int> v;
-	for(int j=0; j<n; j++){
-		if(idx & (1<<j)) v.push_back(j+1);
+void dfs(int k){
+	if(k == m){
+		for(int i=0; i<m; i++){
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return ;
 	}
-	return v;
+	
+	for(int i=1; i<=n; i++){
+		if(!visited[i]){
+			visited[i] = 1;
+			arr[k] = i;
+			dfs(k+1);
+			visited[i] = 0;
+		}
+	}
+	return ;
 }
 
 int main(){
@@ -18,24 +30,7 @@ int main(){
 	cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n >> m;
+	dfs(0);
 	
-	for(int i=1; i<(1<<n); i++){
-		vector<int> v = check(i);
-		if(v.size() == m) ret.push_back(v);
-	}
-	
-	for(auto a : ret){
-		sort(a.begin(), a.end());
-		do{
-			sum.push_back(a);
-		}while(next_permutation(a.begin(), a.end()));
-	}
- 	
-	sort(sum.begin(), sum.end());
-	for(auto a : sum){
-		for(auto i : a) cout << i << " ";
-		cout << "\n";
-	}
- 	
 	return 0;
 }
