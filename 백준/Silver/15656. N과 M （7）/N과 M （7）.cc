574e15a8a9e1bd6784d1f68b2961ccd3f2eca513
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-07-08 16:46:28 +0900

    [Silver III] Title: N과 M (7), Time: 420 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m,arr[15], x ,visited[15];
+vector<int> brr;
+
+void dfs(int d, int cnt){
+	if(cnt == m){
+		for(int i=0; i<m; i++){
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return ;
+	}
+	for(int i=0; i<n; i++){	
+		arr[cnt] = brr[i];
+		dfs(i, cnt+1);
+	}
+	return ;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> m;
+	for(int i=0; i<n; i++){
+		cin >> x;
+		brr.push_back(x);
+	}
+	sort(brr.begin(), brr.end());
+	dfs(0,0);
+	
+	return 0;
+}
