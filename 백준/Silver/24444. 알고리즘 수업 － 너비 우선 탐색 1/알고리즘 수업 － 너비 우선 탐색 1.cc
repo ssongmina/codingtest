0856938c01d13baf739b468938134e5418b5d28d
ssongmina <115204391+ssongmina@users.noreply.github.com>
2025-08-02 09:19:07 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 92 ms, Memory: 9552 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m,r,a,b,cnt=1;
+vector<int> adj[100005];
+int visited[100005];
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> m >> r;
+	for(int i=0; i<m; i++){
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	
+	for(int i=1; i<=n; i++){
+		sort(adj[i].begin(), adj[i].end());
+	}
+	
+	queue<int> q;
+	q.push(r);
+	visited[r] = cnt++;
+	while(q.size()){
+		int here = q.front();
+		q.pop();
+		for(auto there : adj[here]){
+			if(visited[there]) continue;
+			visited[there] = cnt++;
+			q.push(there);
+		}
+	}
+	for(int i=1; i<=n; i++){
+		cout << visited[i] << "\n";
+	}
+	
+	return 0;
+}
