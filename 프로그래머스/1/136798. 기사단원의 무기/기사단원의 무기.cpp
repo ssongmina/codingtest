de0bd312aced04057c356ad01c381e2fcc40efa7
ssongmina <115204391+ssongmina@users.noreply.github.com>
2026-01-16 11:42:48 +0900

    [level 1] Title: 기사단원의 무기, Time: 55.69 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+    for(int i=1; i<=number; i++){
+        int count = 0;
+        for(int j = 1; j * j <= i; ++j) {
+            if(i % j == 0) {
+                if(i / j == j) count++;
+                else count += 2;
+            }
+        }
+        if(count > limit) count = power;
+        answer += count;
+    }
+    return answer;
+}
